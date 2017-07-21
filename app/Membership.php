1833d6b3d37adb86316d644e2340a93f4ad6c68c
mangelosalvio <msalvio.13@gmail.com>
2017-07-21 10:41:45 +0800

    Member delete

--- app/Membership.php
+++ app/Membership.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Membership extends Model
 {
+
+    use SoftDeletes;
 
     protected $fillable = [
         'last_name',
